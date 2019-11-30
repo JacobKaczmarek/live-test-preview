1b603300be810344c1120fb8f5d7aa1d24b3c307
BuildTools <unconfigured@null.spigotmc.org>
2019-11-30 04:25:14 +0100

    wojtek

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const server = require('http').createServer(app)
+const server = require("http").createServer(app);
 
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/start', () => {
-    port.write('420');
-    console.log('Launched');
+app.get("/start", () => {
+  port.write("420");
+  console.log("Launched");
 });
 
-app.get('/stop', () => {
-    port.write('stop');
-    console.log('Stopped');
+app.get("/stop", () => {
+  port.write("stop");
+  console.log("Stopped");
 });
 
-server.listen(PORT, () => console.log('Listening...'))
+server.listen(PORT, () => console.log("Listening..."));
 
-// // Serial port setup
-// const SerialPort = require('serialport');
-// const Readline = SerialPort.parsers.Readline;
-// const port = new SerialPort('', { baudRate: 9600 }, (err) => console.error(err) );
-// const parser = new Readline();
+// Serial port setup
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort("/dev/cu.wchusbserial40110", { baudRate: 9600 }, err => console.error(err));
+const parser = new Readline();
 
-// port.pipe(parser);
+port.pipe(parser);
 
-// port.on('data', (data) => console.log(data));
-
+port.on("data", data => console.log(data.toString()));
 
 // Socket.io setup
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-io.on('connection', (socket) => {
-    
-});
+io.on("connection", socket => {});
